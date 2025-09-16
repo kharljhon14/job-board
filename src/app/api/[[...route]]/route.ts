41124170cce449482bbab621193619892b604a6e
karl <enriquezkharl14@gmail.com>
2025-09-16 11:39:30 +0800

    [Feat] hono api Health api route

---
+++ src/app/api/[[...route]]/route.ts
@@ -0,0 +1,24 @@
+import { Hono } from 'hono';
+import { HTTPException } from 'hono/http-exception';
+import { handle } from 'hono/vercel';
+
+import health from './health';
+
+export const runtime = 'edge';
+
+const app = new Hono().basePath('/api');
+
+app.onError((err, c) => {
+  if (err instanceof HTTPException) {
+    return err.getResponse();
+  }
+  return c.json({ error: 'internal server error' }, 500);
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars, unused-imports/no-unused-vars
+const routes = app.route('/health', health);
+
+export const GET = handle(app);
+export const POST = handle(app);
+
+export type AppType = typeof routes;
