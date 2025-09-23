ec25672c9200266a2f35fc2f8527fea5bc425ee5
karl <enriquezkharl14@gmail.com>
2025-09-24 00:12:03 +0800

    [Feat] Get jobs api metadata

--- src/app/api/[[...route]]/jobs.ts
+++ src/app/api/[[...route]]/jobs.ts
@@ -1,33 +1,65 @@
 import { clerkMiddleware, getAuth } from '@hono/clerk-auth';
 import { zValidator } from '@hono/zod-validator';
 import { createId } from '@paralleldrive/cuid2';
-import { eq } from 'drizzle-orm';
+import { count, eq } from 'drizzle-orm';
 import { Hono } from 'hono';
+import z from 'zod/v4';
 
 import { db } from '@/db/drizzle';
 import { insertJobSchema, jobs, users } from '@/db/schema';
-import { formatZodError } from '@/lib/utils';
+import { formatZodError, generateMetadata } from '@/lib/utils';
 
 const app = new Hono()
-  .get(async (c) => {
-    const data = await db
-      .select({
-        id: jobs.id,
-        userId: jobs.userId,
-        userName: users.name,
-        title: jobs.title,
-        description: jobs.description,
-        salary: jobs.salary,
-        type: jobs.type,
-        status: jobs.status,
-        createdAt: jobs.createdAt,
-        updatedAt: jobs.updatedAt
-      })
-      .from(jobs)
-      .innerJoin(users, eq(jobs.userId, users.id));
+  .get(
+    zValidator(
+      'query',
+      z.object({
+        page: z
+          .string()
+          .regex(RegExp(/^\d+$/), 'page must be a valid number')
+          .optional()
+          .default('1')
+          .transform(Number),
+        pageSize: z
+          .string()
+          .regex(RegExp(/^\d+$/), 'page size must be a valid number')
+          .optional()
+          .default('10')
+          .transform(Number)
+      }),
+      (result, c) => {
+        if (!result.success) {
+          return c.json({ error: formatZodError(result.error) }, 400);
+        }
+      }
+    ),
+    async (c) => {
+      const query = c.req.valid('query');
 
-    return c.json({ data: data });
-  })
+      const totalItems = await db.select({ count: count() }).from(jobs);
+
+      const data = await db
+        .select({
+          id: jobs.id,
+          userId: jobs.userId,
+          userName: users.name,
+          title: jobs.title,
+          description: jobs.description,
+          salary: jobs.salary,
+          type: jobs.type,
+          status: jobs.status,
+          createdAt: jobs.createdAt,
+          updatedAt: jobs.updatedAt
+        })
+        .from(jobs)
+        .limit(query.pageSize)
+        .offset((query.page - 1) * query.pageSize)
+        .innerJoin(users, eq(jobs.userId, users.id));
+
+      const metadata = generateMetadata(totalItems[0].count, query.page, query.pageSize);
+      return c.json({ data, metadata });
+    }
+  )
   .post(
     '/',
     clerkMiddleware(),
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,12 +1,14 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import { ZodError } from 'zod/v4';
 import { $ZodError } from 'zod/v4/core';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function formatZodError(error: $ZodError) {
+export function formatZodError(error: $ZodError<unknown> | ZodError<any>) {
   return error.issues.reduce<Record<string, string>>((acc, issue) => {
     const key = issue.path.join('.') || 'form';
 
@@ -21,4 +23,14 @@ export function convertAmountToMiliunits(amount: number) {
 
 export function convertAmountFromMiliunits(amount: number) {
   return amount / 1000;
+}
+
+export function generateMetadata(totalItems: number, page: number, pageSize: number) {
+  return {
+    currentPage: page,
+    pageSize: pageSize,
+    firstPage: 1,
+    lastPage: Math.ceil(totalItems / pageSize),
+    totalItems
+  };
 }
